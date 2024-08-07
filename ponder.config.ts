c9d73705380ff6def53e5b633697dd718c441a3c
Shekh-Shihab <shekh.shihab@selisegroup.com>
2024-08-07 20:26:53 +0600

    rpc bug fix

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,4 +1,5 @@
 import { createConfig } from "@ponder/core";
+import "dotenv/config";
 import { http, parseAbiItem } from "viem";
 
 import { bsc } from "viem/chains";
@@ -14,38 +15,40 @@ const transport = http(
     ? process.env.PONDER_RPC_URL_1
     : chain.rpcUrls.default.http[0]
 );
+
+console.log(transport);
 const openPositionEvent = parseAbiItem(
   "event PositionOpened(address indexed owner,address indexed position,address ofd,address collateral,uint256 price)"
 );
 
 export default createConfig({
   networks: {
-    mainnet: {
+    bsc: {
       chainId: 56,
       transport,
     },
   },
   contracts: {
     OracleFreeDollar: {
-      network: "mainnet",
+      network: "bsc",
       abi: OracleFreeDollar,
       address: "0x55899A4Cd6D255DCcAA84d67E3A08043F2123d7E",
       startBlock: 37881973,
     },
     Equity: {
-      network: "mainnet",
+      network: "bsc",
       abi: Equity,
       address: "0xeA38b0cD48fA781181FDAa37291e8d6668462261",
       startBlock: 37881973,
     },
     MintingHub: {
-      network: "mainnet",
+      network: "bsc",
       abi: MintingHub,
       address: "0xFe00054AF44E24f0B4bd49b1A2d2984C4264aabE",
       startBlock: 37882077,
     },
     Position: {
-      network: "mainnet",
+      network: "bsc",
       abi: Position,
       factory: {
         address: "0xFe00054AF44E24f0B4bd49b1A2d2984C4264aabE",
